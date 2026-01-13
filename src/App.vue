e95fc00616c92d77e46ee1eec9dfe2f2a292aa2b
v0l0dar <8081@ukr.net>
2026-01-13 19:29:09 +0200

    init project with settings

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useStore } from 'vuex'
+
+const store = useStore()
+
+const horses = computed(() => store.state.horses)
+</script>
+
+<template>
+  <header>Header</header>
+
+  <main>
+    <h2>Horses</h2>
+
+    <ul>
+      <li v-for="horse in horses" :key="horse">
+        {{ horse }}
+      </li>
+    </ul>
+  </main>
+</template>
